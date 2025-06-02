da914cb6115125bdb665c612ec3276324cac24d2
Samir Arana <samirivan.arana@alumnos.ulagos.cl>
2025-06-02 16:27:25 -0400

    añadiendo notas al ejercicio 4

--- Ejercicios_practica__python/Ejercicio4.py
+++ Ejercicios_practica__python/Ejercicio4.py
@@ -18,10 +18,11 @@ D. Crear una variable booleana llamada umbral, que entregue un True si el valor_
 E. Imprimir el nombre del producto, la cantidad, el valor total y el estado umbral en un solo
 print() formateado
 '''
+#se piden las diferentes variables para su uso posterior 
 nombre = input('Nombre del producto: ')
 precio = float(input('Precio unitario: '))
 cantidad = int(input('Cantidad en stock: '))
-
+#formula para saber el total
 total = precio * cantidad
-
+#se imprimen los diferentes resultados y si es mayo o menor a 100000 con un if else ( la condicion va al medio )
 print(f'{nombre} - Cantidad: {cantidad}, Total: ${total:.2f}, Estado: {"Mayor a 100000" if total > 100000 else "Menor o igual a 100000"}')
