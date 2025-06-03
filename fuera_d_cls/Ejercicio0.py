abe70933b5072c9ef61fa1d4f19531c37029b95e
Samir Arana <samirivan.arana@alumnos.ulagos.cl>
2025-06-02 21:26:19 -0400

    haciendo ejercicios faciles fuera de clases

---
+++ fuera_d_cls/Ejercicio0.py
@@ -0,0 +1,11 @@
+'''
+Lucas tiene 50 naranjas. Decide repartirlas entre Abril y Mateo.
+Si Abril recibe 20 y Mateo recibe 15, ¿cuántas le quedan a Lucas?
+'''
+
+lucas = 50 
+mateo = 15
+abril = 20 
+lucas_dsp = lucas - (abril + mateo)
+
+print(f'a Lucas le quedan, {lucas_dsp} naranjas')
---
+++ fuera_d_cls/ejercicio1.py
@@ -0,0 +1,8 @@
+'''
+Abril tiene 12 manzanas, Mateo tiene 8 y Lucas tiene 15.
+Mostrá quién tiene más frutas y cuántas tiene.
+'''
+
+abril = 12 
+mateo = 8 
+lucas = 15
