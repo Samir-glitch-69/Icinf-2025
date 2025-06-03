099643db1ee96d0ce07094f5871f27b80c15fd26
Samir Arana <samirivan.arana@alumnos.ulagos.cl>
2025-06-02 21:27:25 -0400

    cambio de texto repetido

--- Ejercicios_practica__python/Ejercicio4.py
+++ Ejercicios_practica__python/Ejercicio4.py
@@ -8,16 +8,7 @@ D. Crear una variable booleana llamada umbral, que entregue un True si el valor_
 E. Imprimir el nombre del producto, la cantidad, el valor total y el estado umbral en un solo
 print() formateado
 '''
-'''
-4. Desarrollar un programa de gestión de inventario:
-A. Ingresar el nombre de un producto y su precio unitario.
-B. Ingresar la cantidad en stock.
-C. Calcular el valor total de los productos ingresados y mostrarlo con 2 decimales.
-D. Crear una variable booleana llamada umbral, que entregue un True si el valor_total >
-100000 y False en caso contrario..
-E. Imprimir el nombre del producto, la cantidad, el valor total y el estado umbral en un solo
-print() formateado
-'''
+
 #se piden las diferentes variables para su uso posterior 
 nombre = input('Nombre del producto: ')
 precio = float(input('Precio unitario: '))
