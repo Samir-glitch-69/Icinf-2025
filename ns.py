85c0e4df107060ce3aa280c9ceb1c022bc9ffacc
Samir Arana <samirivan.arana@alumnos.ulagos.cl>
2025-06-16 21:43:48 -0400

    borrando ns.py y creando carpeta de ejercicios hechos en pseint para crearlos en .py

--- ns.py
+++
@@ -1,9 +0,0 @@
-#se pide un numero entero
-n = int(input("Ingresa un número: "))
-#se define resultado como 1
-resultado = 1
-
-for i in range(1, n + 1):
-    resultado *= i
-
-print(f"El factorial de {n} es {resultado}")
